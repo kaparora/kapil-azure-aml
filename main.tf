03172a48169be4d080e7be868ce0ecd2bbed13f4
Kapil Arora <512729+kaparora@users.noreply.github.com>
2021-01-20 13:28:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,12 @@ module "machine-learning" {
   resource_group = "kapil-aml"
   location = "northeurope"
 }
+  
+output "jumphost_ip" {
+  value = module.machine-learning.jumphost_ip
+}
+  
+  
+output "machine_learning_workspace_name" {
+  value = module.machine-learning.machine_learning_workspace_name
+}
